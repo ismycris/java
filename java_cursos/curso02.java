484b6b23569c53b02bb92c2b604884e2bbd5a645
ismycris <cristielythainara@gmail.com>
2024-01-29 16:46:45 -0300

    AND

--- java_cursos/curso02.java
+++ java_cursos/curso02.java
@@ -4,9 +4,17 @@ public class curso02 {
         //==  >  <  >=  <=  !=  !(true/false)
 
         int nota = 70 ;
+        int faltas = 10;
+        int maxFaltas = 5;
         int media = 60;
+        //tabela verdade AND
+        /*  v v = v
+            v f = f
+            f v = f
+            f f = f
+         */
 
-        if (nota >= media) {
+        if (nota >= media && faltas<= maxFaltas) {
             System.out.println("aprovado");
         }else if (nota >= 40) {
             System.out.println("recuperacao");
