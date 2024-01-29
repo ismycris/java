7cd99a8a9e512dd171341f70e582accb0349f97e
ismycris <cristielythainara@gmail.com>
2024-01-29 16:00:28 -0300

    update

---
+++ cursojava/001.java
@@ -0,0 +1,19 @@
+public class cursojava {
+    public static void main (String[] args){
+
+        int n1=0;
+        int n2= 20;
+        int n3=30;
+        int res=n1+n2+n3;
+
+        int num=100;
+        String nome="banana";
+        num =50;
+
+        System.out.print("jurema\n");
+        System.out.println("curso de java");
+        System.out.printf(" canal:%s ano:%d%n","jurema ", 2024 );
+        System.out.printf(" valor da variavel num:%d %n nome:%s",num,nome);
+        System.out.printf("valor da variável res: %d%n", res);
+    }
+}
