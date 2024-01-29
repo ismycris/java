b7ff06adc187d1cc3e9b6362f5199fe9789ae85c
ismycris <cristielythainara@gmail.com>
2024-01-29 15:34:19 -0300

    update

--- cursojava/cursojava.java
+++ cursojava/cursojava.java
@@ -1,5 +1,7 @@
 public class cursojava {
     public static void main (String[] args){
+        System.out.print("jurema\n");
         System.out.println("curso de java");
+        System.out.printf(" canal:%s ano:%d","jurema", 2024);
     }
 }
